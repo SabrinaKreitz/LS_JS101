430f2ff661b7c62a1849ae5760d5e082711827a6
Sabrina Kreitz <sabrinakreitz.s@gmail.com>
2024-02-06 18:55:21 +0100

    best out of five addition to rps game

--- lesson_2/rock_paper_scissors.js
+++ lesson_2/rock_paper_scissors.js
@@ -8,6 +8,8 @@ and announces the winner after one of the players achieved three wins.
 
 E
 D
+Input:
+Output:
 
 A
 Create a loop and within the loop, set two counter variables
@@ -21,6 +23,9 @@ The loop stops when one of the conditions equals 3.
 
 const readline = require('readline-sync');
 const VALID_CHOICES = ['rock', 'paper', 'scissors', 'spock', 'lizard'];
+const WINNING_POINTS = 3;
+let userPoints = 0;
+let computerPoints = 0;
 
 function userWins(choice, computerChoice) {
   if ((choice === 'rock' && (computerChoice === ('scissors') || computerChoice === ('lizard'))) ||
@@ -45,48 +50,71 @@ function computerWins(choice, computerChoice) {
     return false;
   }
 }
-function gameEnds() {
-  let userWins = 0;
-  let computerWins = 0;
-  while (userWins < 3 || computerWins < 3) {
-  if(userWins(choice, computerChoice)) {
-    userWins += 1;
-  } else (computerWins(choice, computerChoice)) {
-    computerWins += 1;
+function gameEnds(choice, computerChoice) {
+  if (userWins(choice, computerChoice)) {
+    userPoints += 1;
+    console.log(`Your score is ${userPoints} :)`);
+  } else if (computerWins(choice, computerChoice)) {
+    computerPoints += 1;
+    console.log(`Computers score is ${computerPoints} :()`);
   }
 }
+
+function displayWinnerRound(choice, computerChoice) {
+  prompt(`You chose ${choice}, computer chose ${computerChoice}`);
+
+  if (userWins(choice, computerChoice)) {
+    prompt(`You won this round.`);
+  } else if (computerWins(choice, computerChoice)) {
+    prompt(`Computer wins this round.`);
+  } else {
+    prompt("It's a tie!");
+  }
+}
+
+function displayWinnerGame() {
+  if (userPoints === WINNING_POINTS) {
+    console.log('You won the game!');
+  } else if (computerPoints === WINNING_POINTS) {
+    console.log('You lost the game!');
+  }
+}
+
+function resetPoints() {
+  userPoints = 0;
+  computerPoints = 0;
 }
 
 function prompt(message) {
   console.log(`=> ${message}`);
 }
 
-/* function displayWinner(choice, computerChoice) {
-  prompt(`You chose ${choice}, computer chose ${computerChoice}`);
-
-  if (userWins(choice, computerChoice)) {
-    prompt('You win!');
-  } else if (computerWins(choice, computerChoice)) {
-    prompt('Computer wins!');
-  } else {
-    prompt("It's a tie!");
-  }
-} */
 
 while (true) {
   console.clear();
-  prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
-  let choice = readline.question();
+  while (true) {
+    prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
+    let choice = readline.question();
 
-  while (!VALID_CHOICES.includes(choice)) {
-    prompt("That's not a valid choice");
-    choice = readline.question();
+    while (!VALID_CHOICES.includes(choice)) {
+      prompt("That's not a valid choice");
+      choice = readline.question();
+    }
+
+    let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
+    let computerChoice = VALID_CHOICES[randomIndex];
+
+    displayWinnerRound(choice, computerChoice);
+
+    gameEnds(choice, computerChoice);
+
+    if (userPoints === WINNING_POINTS ||
+    computerPoints === WINNING_POINTS) break;
   }
 
-  let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
-  let computerChoice = VALID_CHOICES[randomIndex];
+  displayWinnerGame();
 
-  displayWinner(choice, computerChoice);
+  resetPoints();
 
   prompt('Do you want to play again (y/n)?');
   let answer = readline.question().toLowerCase();
